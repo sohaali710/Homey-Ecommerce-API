e3d28538573106f2f5f98a3e6bb148f94e0fa4c5
Soha <sohaali710@gmail.com>
2023-08-01 03:40:43 +0300

    commit

--- routers/cart.js
+++ routers/cart.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Cart = require("../models/cart");
+const Cart = require("../models/Cart");
 const Product = require('../models/Product')
 const Auth = require("../middleware/auth");
 
